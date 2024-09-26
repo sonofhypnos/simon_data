64a67caf68741d529d5a65409c95aaadaac29357
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2024-09-26 16:33:24 +0200

    Cleanup source and update docs

--- process_sun_data.py
+++ process_sun_data.py
@@ -6,38 +6,44 @@ from collections import defaultdict
 start_of_workday = 8
 end_of_workday = 13
 
+
 def estimate_sun_hour(hour_data):
-    dt = datetime.fromtimestamp(hour_data['dt'])
-    sunrise = datetime.fromtimestamp(hour_data['sunrise'])
-    sunset = datetime.fromtimestamp(hour_data['sunset'])
-    
+    dt = datetime.fromtimestamp(hour_data["dt"])
+    sunrise = datetime.fromtimestamp(hour_data["sunrise"])
+    sunset = datetime.fromtimestamp(hour_data["sunset"])
+
     if sunrise <= dt <= sunset:
-        cloud_cover = hour_data['clouds']
+        cloud_cover = hour_data["clouds"]
         # Estimate sun hour based on cloud cover
         return 1 - (cloud_cover / 100)
     return 0
 
+
 def process_weather_data_to_csv(input_files, output_file, daily_output_file):
     hourly_data = defaultdict(lambda: defaultdict(float))
-    
+
     for input_file in input_files:
         with open(input_file, "r") as json_file:
             data = json.load(json_file)
             for entry in data:
-                hour_data = entry['data'][0]  # Access the single entry in the 'data' list
-                
-                dt = datetime.fromtimestamp(hour_data['dt'])
+                hour_data = entry["data"][
+                    0
+                ]  # Access the single entry in the 'data' list
+
+                dt = datetime.fromtimestamp(hour_data["dt"])
                 date = dt.strftime("%Y-%m-%d")
                 hour = dt.strftime("%H")
-                
+
                 sun_hour = estimate_sun_hour(hour_data)
-                hourly_data[date][hour] = max(hourly_data[date][hour], sun_hour)  # Keep the maximum value in case of duplicates
+                hourly_data[date][hour] = max(
+                    hourly_data[date][hour], sun_hour
+                )  # Keep the maximum value in case of duplicates
 
     # Write hourly data
     with open(output_file, mode="w", newline="") as file:
         writer = csv.writer(file)
         writer.writerow(["Date", "Hour", "Estimated Sun Hour"])
-        
+
         for date in sorted(hourly_data.keys()):
             for hour in sorted(hourly_data[date].keys()):
                 writer.writerow([date, hour, round(hourly_data[date][hour], 2)])
@@ -48,12 +54,8 @@ def process_weather_data_to_csv(input_files, output_file, daily_output_file):
     with open(daily_output_file, mode="w", newline="") as file:
         writer = csv.writer(file)
         writer.writerow(["Date", "Total Estimated Sun Hours"])
-        
-        for date in sorted(hourly_data.keys()):
-            # Prediction:
-            # - If there is a higher correlation between the time where Simon actually works, this will be some evidence that the stuff is not random
-            # If the correlation is lower, then this is an indication that it is somewhat random and that simon might be choosing the days depending no the weather that day (although only weak for choosing day by weather maybe)
 
+        for date in sorted(hourly_data.keys()):
             total_sun_hours = 0
             for hour in hourly_data[date]:
                 if int(hour) >= start_of_workday and int(hour) <= end_of_workday:
@@ -62,11 +64,16 @@ def process_weather_data_to_csv(input_files, output_file, daily_output_file):
 
     print("Daily CSV file has been created with aggregated sun hours.")
 
+
 # Specify the input JSON files and output CSV file paths
 input_json_files = [
+    # NOTE: duplicate data does not lead to problems. Data should overlap by 2 days (or you need to pay close attention until which hour the data goes)
     "data/weather/weather_data_2024-05-01_2024-06-01.json",
     "data/weather/weather_data_2024-06-01_2024-07-01.json",
-    "data/weather/weather_data_2024-06-30_2024-07-29.json"
+    "data/weather/weather_data_2024-06-30_2024-07-29.json",
+    "data/weather/weather_data_2024-07-08_2024-08-06.json",
+    "data/weather/weather_data_2024-07-30_2024-08-31.json",
+    "data/weather/weather_data_2024-08-25_2024-09-23.json",
 ]
 output_csv_file = "data/weather/estimated_hourly_sun_hours.csv"
 daily_output_csv_file = "data/weather/estimated_daily_sun_hours_during_work.csv"
--- readme.md
+++ readme.md
@@ -6,7 +6,26 @@ Other code uses normal .env/
 Explanation:
 - Has analysis and data for simons productivity data
 - the ground truth for simons productivity data is supposed to stay the online google sheets file. We only read and wrangle this data for analysis
-- has code to extract and modify RC-5 thermometer
-- has a weather script to get data from weather place
+- has code to extract and modify RC-5 thermometer (this code did not end up working due to hardware issues that could never be resolved)
+- has a weather script to get data from open_weather.
+
+# General information:
+- There are two main downstream 'variables' that interest us: Simon's sleep and Simon's productivity.
+- There are two functions to calculate effects for each of these variables respectively. The notebook also has a lot of cells for data cleaning and formatting in the beginning
+
+# How to update the open weather data
+Info:
+- openweather allows a limit of 1K api calls per day. Since 24*30=720, you can only download 1 day worth of api calls for free per day. You also need an api key from them to run these functions at all I believe. The repository has code to fetch the api key from the ignored .env file.
+
+Steps:
+- [ ] download data (until you have all data for last months)
+ - [ ] open `fetch_open_weather.py`
+ - [ ] change the variables for `days_since`, and `days_forward` to determine for which time_window you want to download data
+ - [ ] run `fetch_open_weather.py` (Make sure your data overlaps by 2 days with previous data or you might get missing data within 1 day!)
+- [ ] turn the data to csv data:
+  - [ ] open `process_sun_data.py`
+  - [ ] add your files to the `input_json_files` variable
+  - [ ] run `process_sun_data.py`
+  - [ ] possibly there is duplicate data that you need to manually deal with?
 
 
